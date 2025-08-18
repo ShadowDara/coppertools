46ea744309dc10fec41f722e4a3d1d70d50faef6
ShadowDara <128976697+ShadowDara@users.noreply.github.com>
2025-08-18 16:24:57 +0200

    Publish

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # Copper Tools
 
+### [Download here](https://modrinth.com/mod/easycoppertools)
+
 a simple mod for 1.20.1 which adds coppertools
 to the minecraft game
 
 btw i make this mod for one of my modpacks, just for you to know.
+
+## **Copper Armor Mod**
+This mod adds a brand new set of copper equipment to Minecraft! Craft copper armor and tools to bridge the gap between stone and iron. Copper gear offers moderate protection and durability, giving players an early-game upgrade while making use of the abundant copper resource.
--- src/main/java/de/shadowdara/coppertools/item/ModItems.java
+++ src/main/java/de/shadowdara/coppertools/item/ModItems.java
@@ -4,9 +4,7 @@ import de.shadowdara.coppertools.Coppertools;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
-import net.minecraft.item.ArmorItem;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroups;
+import net.minecraft.item.*;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
@@ -29,11 +27,43 @@ public class ModItems {
                     new FabricItemSettings())
     );
 
+    public static final Item COPPER_SWORD = registerItem("copper_sword",
+            new SwordItem(ModToolMaterial.COPPER, 3, -2.4f,
+            new FabricItemSettings())
+    );
+    public static final Item COPPER_PICKAXE = registerItem("copper_pickaxe",
+            new PickaxeItem(ModToolMaterial.COPPER, 1, -2.8f,
+            new FabricItemSettings())
+    );
+    public static final Item COPPER_AXE = registerItem("copper_axe",
+            new AxeItem(ModToolMaterial.COPPER, 6.5f, -3.2f,
+            new FabricItemSettings())
+    );
+    public static final Item COPPER_SHOVEL = registerItem("copper_shovel",
+            new ShovelItem(ModToolMaterial.COPPER, 1.5f, -3f,
+            new FabricItemSettings())
+    );
+    public static final Item COPPER_HOE = registerItem("copper_hoe",
+            new HoeItem(ModToolMaterial.COPPER, -2, -1.5f,
+            new FabricItemSettings())
+    );
+
+
     private static void addItemsToArmorItemGroup(FabricItemGroupEntries entries) {
         entries.add(COPPER_HELMET);
         entries.add(COPPER_CHESTPLATE);
         entries.add(COPPER_LEGGINGS);
         entries.add(COPPER_BOOTS);
+
+        entries.add(COPPER_SWORD);
+        entries.add(COPPER_AXE);
+    }
+
+    private static void addItemsToToolsGroup(FabricItemGroupEntries entries) {
+        entries.add(COPPER_PICKAXE);
+        entries.add(COPPER_AXE);
+        entries.add(COPPER_SHOVEL);
+        entries.add(COPPER_HOE);
     }
 
     private static Item registerItem(String name, Item item) {
@@ -46,5 +76,6 @@ public class ModItems {
         Coppertools.LOGGER.info("Registering Mod Items for " + Coppertools.MOD_ID);
 
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.COMBAT).register(ModItems::addItemsToArmorItemGroup);
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.TOOLS).register(ModItems::addItemsToToolsGroup);
     }
 }
