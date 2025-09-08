31f66d45a8354b9fbf32ae88257bc413b6a34940
ShadowDara <128976697+ShadowDara@users.noreply.github.com>
2025-09-08 17:37:48 +0200

    add recipes

--- src/main/java/de/shadowdara/coppertools/datagen/ModRecipeProvider.java
+++ src/main/java/de/shadowdara/coppertools/datagen/ModRecipeProvider.java
@@ -7,7 +7,6 @@ import net.minecraft.data.server.recipe.RecipeJsonProvider;
 import net.minecraft.data.server.recipe.ShapedRecipeJsonBuilder;
 import net.minecraft.item.Items;
 import net.minecraft.recipe.book.RecipeCategory;
-import net.minecraft.util.Identifier;
 
 import java.util.function.Consumer;
 
@@ -22,9 +21,75 @@ public class ModRecipeProvider extends FabricRecipeProvider {
                 .pattern("SSS")
                 .pattern("S S")
                 .input('S', Items.COPPER_INGOT)
-                .criterion(hasItem(Items.COPPER_INGOT),
-                        conditionsFromItem(Items.COPPER_INGOT))
+                .criterion("has_copper_ingot", conditionsFromItem(Items.COPPER_INGOT))
                 .offerTo(exporter);
 
+        ShapedRecipeJsonBuilder.create(RecipeCategory.COMBAT, ModItems.COPPER_CHESTPLATE)
+                .pattern("S S")
+                .pattern("SSS")
+                .pattern("SSS")
+                .input('S', Items.COPPER_INGOT)
+                .criterion("has_copper_ingot", conditionsFromItem(Items.COPPER_INGOT))
+                .offerTo(exporter);
+
+        ShapedRecipeJsonBuilder.create(RecipeCategory.COMBAT, ModItems.COPPER_LEGGINGS)
+                .pattern("SSS")
+                .pattern("S S")
+                .pattern("S S")
+                .input('S', Items.COPPER_INGOT)
+                .criterion("has_copper_ingot", conditionsFromItem(Items.COPPER_INGOT))
+                .offerTo(exporter);
+
+        ShapedRecipeJsonBuilder.create(RecipeCategory.COMBAT, ModItems.COPPER_BOOTS)
+                .pattern("S S")
+                .pattern("S S")
+                .input('S', Items.COPPER_INGOT)
+                .criterion("has_copper_ingot", conditionsFromItem(Items.COPPER_INGOT))
+                .offerTo(exporter);
+
+        ShapedRecipeJsonBuilder.create(RecipeCategory.COMBAT, ModItems.COPPER_SWORD)
+                .pattern(" S ")
+                .pattern(" S ")
+                .pattern(" X ")
+                .input('S', Items.COPPER_INGOT)
+                .input('X', Items.STICK)
+                .criterion("has_copper_ingot", conditionsFromItem(Items.COPPER_INGOT))
+                .offerTo(exporter);
+
+        ShapedRecipeJsonBuilder.create(RecipeCategory.COMBAT, ModItems.COPPER_PICKAXE)
+                .pattern("SSS")
+                .pattern(" X ")
+                .pattern(" X ")
+                .input('S', Items.COPPER_INGOT)
+                .input('X', Items.STICK)
+                .criterion("has_copper_ingot", conditionsFromItem(Items.COPPER_INGOT))
+                .offerTo(exporter);
+
+        ShapedRecipeJsonBuilder.create(RecipeCategory.COMBAT, ModItems.COPPER_AXE)
+                .pattern("SS ")
+                .pattern("SX ")
+                .pattern(" X ")
+                .input('S', Items.COPPER_INGOT)
+                .input('X', Items.STICK)
+                .criterion("has_copper_ingot", conditionsFromItem(Items.COPPER_INGOT))
+                .offerTo(exporter);
+
+        ShapedRecipeJsonBuilder.create(RecipeCategory.COMBAT, ModItems.COPPER_SHOVEL)
+                .pattern(" S ")
+                .pattern(" X ")
+                .pattern(" X ")
+                .input('S', Items.COPPER_INGOT)
+                .input('X', Items.STICK)
+                .criterion("has_copper_ingot", conditionsFromItem(Items.COPPER_INGOT))
+                .offerTo(exporter);
+
+        ShapedRecipeJsonBuilder.create(RecipeCategory.COMBAT, ModItems.COPPER_HOE)
+                .pattern("SS ")
+                .pattern(" X ")
+                .pattern(" X ")
+                .input('S', Items.COPPER_INGOT)
+                .input('X', Items.STICK)
+                .criterion("has_copper_ingot", conditionsFromItem(Items.COPPER_INGOT))
+                .offerTo(exporter);
     }
 }
